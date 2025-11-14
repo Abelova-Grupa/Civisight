9350e9f9dabd9afb0596f2c03deb75a5d75fa9ec
jelisavac-l <jelisavacluka63@gmail.com>
2025-11-14 14:08:02 +0200

    feat(backend): setup

---
+++ backend/src/main/java/com/abeliangroup/civisight/CivisightApplication.java
@@ -0,0 +1,13 @@
+package com.abeliangroup.civisight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivisightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivisightApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/abeliangroup/civisight/CivisightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abeliangroup.civisight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivisightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
